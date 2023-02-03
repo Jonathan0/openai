e387629fac0db0285efd1333e33b0f7b477367b2
Jonathan0 <johnlin989@gmail.com>
2023-02-02 20:38:03 -0500

    enable loop testing

--- cypress/e2e/openai.cy.js
+++ cypress/e2e/openai.cy.js
@@ -1,12 +1,17 @@
 describe('openAI animal name test', () => {
 
   it('visit the openAI local home page', () => {
+    const animal = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"];
     cy.on('uncaught:exception', (err, runnable) => { return false; });
     cy.visit('http://localhost:3000');
-    cy.get('input[name="animal"]').type('dog');
-    cy.get('#__next > div > main > form > input[type=text]:nth-child(1)').should('have.value', 'dog');
-    cy.get('#__next > div > main > form > input[type=submit]:nth-child(2)').click();
-    cy.wait(1000);
-    cy.get('#__next > div > main > div').should('be.visible');
+    for(let i=0; i<20; i++) {
+        cy.wait(1000);
+        const name = animal[Math.floor(Math.random() * 12)];
+        cy.get('input[name="animal"]').clear().type(name);
+        cy.get('#__next > div > main > form > input[type=text]:nth-child(1)').should('have.value', name);
+        cy.get('#__next > div > main > form > input[type=submit]:nth-child(2)').click();
+        cy.wait(1000);
+        cy.get('#__next > div > main > div').should('be.visible');
+    }
   })
 })
