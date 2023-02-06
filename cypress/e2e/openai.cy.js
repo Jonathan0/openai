8b83bb7510dcdab19414b68837c5cadf7bb3812c
Jonathan0 <johnlin989@gmail.com>
2023-02-05 21:22:40 -0500

    feat: update cypress tests

--- cypress/e2e/openai.cy.js
+++ cypress/e2e/openai.cy.js
@@ -6,9 +6,12 @@ describe('openAI country places test', () => {
       cy.fixture('countries').then((countries) => {
         this.countries = countries
       })
+      cy.fixture('questions').then((questions) => {
+        this.questions = questions
+      })
     });
 
-  it('visit the openAI local home page', function () {
+  it('openAI search country test', function () {
     const country = this.countries.names;
     let dictionary = "";
 
@@ -18,11 +21,11 @@ describe('openAI country places test', () => {
         cy.wait(500);
         const name = country[Math.floor(Math.random() * country.length)];
         cy.get('input[name="country"]').clear().type(name);
-        cy.get('#__next > div > main > form > input[type=text]:nth-child(1)').should('have.value', name);
-        cy.get('#__next > div > main > form > input[type=submit]:nth-child(2)').click();
+        cy.get('#__next > div > main > form:nth-child(3) > input[type=text]:nth-child(1)').should('be.visible');
+        cy.get('#__next > div > main > form:nth-child(3) > input[type=submit]:nth-child(2)').click();
         cy.wait(1500);
-        cy.get('#__next > div > main > div').should('be.visible');
-        cy.get('#__next > div > main > div').then($value => {
+        cy.get('#__next > div > main > div:nth-child(4)').should('be.visible');
+        cy.get('#__next > div > main > div:nth-child(4)').then($value => {
             // save to file
             const places = $value.text();
             cy.writeFile('cypress/json/' + name + "Name.json", places, 'utf8');
@@ -30,4 +33,25 @@ describe('openAI country places test', () => {
     }
   })
 
+  it('openAI find answers test', function () {
+      const question = this.questions.lists;
+      let dictionary = "";
+
+      cy.on('uncaught:exception', (err, runnable) => { return false; });
+      cy.visit('http://localhost:3000');
+      for(let i=0; i<5; i++) {
+          cy.wait(500);
+          const name = question[Math.floor(Math.random() * question.length)];
+          cy.get('input[name="question"]').clear().type(name);
+          cy.get('#__next > div > main > form:nth-child(7) > input[type=text]:nth-child(1)').should('be.visible');
+          cy.get('#__next > div > main > form:nth-child(7) > input[type=submit]:nth-child(2)').click();
+          cy.wait(1500);
+          cy.get('#__next > div > main > div:nth-child(8)').should('be.visible');
+          cy.get('#__next > div > main > div:nth-child(8)').then($value => {
+              // save to file
+              const ans = $value.text();
+              cy.writeFile('cypress/json/' + name.slice(-10,-1) + "Answer.json", ans, 'utf8');
+          })
+      }
+    })
 })
