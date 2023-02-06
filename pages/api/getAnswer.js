9096a30489fd263fba18b3b44fad24212f351777
Jonathan0 <johnlin989@gmail.com>
2023-02-05 20:53:21 -0500

    feat: new api for answering questions

---
+++ pages/api/getAnswer.js
@@ -0,0 +1,87 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const question = req.body.question || '';
+  if (question.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid question",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(question),
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\n"],
+    });
+    res.status(200).json({ answer: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(question) {
+  const capitalizedquestion = question.toUpperCase();
+    // question[0].toUpperCase() + question.slice(1).toLowerCase();
+  return `I am a highly intelligent question answering bot.
+
+If you ask me a question that is rooted in truth, I will give you the answer.
+If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".
+
+Q: What is human life expectancy in the United States?
+A: Human life expectancy in the United States is 78 years.
+
+Q: Who was president of the United States in 1955?
+A: Dwight D. Eisenhower was president of the United States in 1955.
+
+Q: Which party did he belong to?
+A: He belonged to the Republican Party.
+
+Q: What is the square root of banana?
+A: Unknown
+
+Q: How does a telescope work?
+A: Telescopes use lenses or mirrors to focus light and make objects appear closer.
+
+Q: Where were the 1992 Olympics held?
+A: The 1992 Olympics were held in Barcelona, Spain.
+
+Q: How many squigs are in a bonk?
+A: Unknown
+
+Q: ${capitalizedquestion}
+A:`;
+}
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ export default function Home() {
   const [countryInput, setCountryInput] = useState("");
   const [result, setResult] = useState();
 
+  const [questionInput, setQuestionInput] = useState("");
+  const [answer, setAnswer] = useState();
+
   async function onSubmit(event) {
     event.preventDefault();
     try {
@@ -31,6 +34,30 @@ export default function Home() {
     }
   }
 
+  async function onSubmit2(event) {
+      event.preventDefault();
+      try {
+        const response = await fetch("/api/getAnswer", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ question: questionInput }),
+        });
+
+        const data = await response.json();
+        if (response.status !== 200) {
+          throw data.error || new Error(`Request failed with status ${response.status}`);
+        }
+
+        setAnswer(data.answer);
+        setQuestionInput("");
+      } catch(error) {
+        console.error(error);
+        alert(error.message);
+      }
+    }
+
   return (
     <div>
       <Head>
@@ -52,6 +79,20 @@ export default function Home() {
           <input type="submit" value="Search places" />
         </form>
         <div className={styles.result}>{result}</div>
+
+        <img src="/qst.png" className={styles.icon} />
+        <h3>Find the answer</h3>
+        <form onSubmit={onSubmit2}>
+          <input
+            type="text"
+            name="question"
+            placeholder="Enter a question"
+            value={questionInput}
+            onChange={(e) => setQuestionInput(e.target.value)}
+          />
+          <input type="submit" value="Search answer" />
+        </form>
+        <div className={styles.result}>{answer}</div>
       </main>
     </div>
   );
